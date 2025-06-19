5d71f36b81d6b949a85462359f15a8be5913321e
Hiroki1025 <kasaihiroki1025@gmail.com>
2025-06-19 23:23:10 +0900

    kk

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse #インポート
+
+### コードいろいろ... ###
+
+@app.get("/index")
+def index():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
 app = FastAPI()
 
@@ -13,8 +30,9 @@ async def root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("omikuji")
+@app.get("/omikuji")
 def omikuji():
+
     omikuji_list = [
         "大吉",
         "中吉",
@@ -28,4 +46,6 @@ def omikuji():
         "大凶"
     ]
     return omikuji_list[random.randrange(10)]
+
+
 
